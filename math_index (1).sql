c2dea13bf8287c00ec458d729c4b70f04dd690dd
SashaCarton <163831379+SashaCarton@users.noreply.github.com>
2024-03-24 15:41:25 +0100

    v1.2 avec utilisateurs

---
+++ math_index (1).sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 24 mars 2024 à 15:40
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `math_index`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `exercices`
+--
+
+CREATE TABLE `exercices` (
+  `ID` int(11) NOT NULL,
+  `Nom` varchar(100) NOT NULL,
+  `Thematique` varchar(100) NOT NULL,
+  `Difficulte` int(11) DEFAULT NULL,
+  `Duree` time DEFAULT NULL,
+  `MotsCles` varchar(255) DEFAULT NULL,
+  `Fichier` longblob DEFAULT NULL,
+  `MatiereID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `exercices`
+--
+
+INSERT INTO `exercices` (`ID`, `Nom`, `Thematique`, `Difficulte`, `Duree`, `MotsCles`, `Fichier`, `MatiereID`) VALUES
+(1, 'Exo suite', 'Suites', 8, '04:00:00', 'arithmétique, base', NULL, 1),
+(2, 'Primitives', 'Continuité', 11, '01:00:00', 'récurrence', NULL, 2),
+(3, 'Continuité diverse', 'Continuité', 12, '02:00:00', 'arithmétique', NULL, 2),
+(4, 'Matrice à gogo', 'Matriciel', 11, '03:00:00', 'base', NULL, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matieres`
+--
+
+CREATE TABLE `matieres` (
+  `ID` int(11) NOT NULL,
+  `Nom` varchar(100) NOT NULL,
+  `Description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `matieres`
+--
+
+INSERT INTO `matieres` (`ID`, `Nom`, `Description`) VALUES
+(1, 'Suites', 'Exercices sur les suites arithmétiques et géométriques.'),
+(2, 'Continuité', 'Exercices sur la continuité des fonctions.'),
+(3, 'Matriciel', 'Exercices sur les opérations matricielles.');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `mot_de_passe` varchar(100) DEFAULT NULL,
+  `niveau_droit` enum('eleve','professeur','administrateur') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `exercices`
+--
+ALTER TABLE `exercices`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `MatiereID` (`MatiereID`);
+
+--
+-- Index pour la table `matieres`
+--
+ALTER TABLE `matieres`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `exercices`
+--
+ALTER TABLE `exercices`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `matieres`
+--
+ALTER TABLE `matieres`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `exercices`
+--
+ALTER TABLE `exercices`
+  ADD CONSTRAINT `exercices_ibfk_1` FOREIGN KEY (`MatiereID`) REFERENCES `matieres` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
