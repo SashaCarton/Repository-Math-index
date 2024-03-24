b87f344a96627a4c0d5eb26db3e4250724295751
Noah <noah.bonnaventure@outlook.com>
2024-03-24 14:20:58 +0100

    implémentation php pour slide-bar
    la slide-bar a été mise a part avec un fichier php et css spécifique a celle dernière.
    A partir d'ici, la slide bar est intégré au site avec la fonction require de php

---
+++ components/slide-bar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/css/style-slide-bar.css" rel="stylesheet">
+    <title></title>
+</head>
+<body>
+    <div class="slideBar"> <!--For the bar on the left -->
+        <div class="slidetitle">
+            <img src="assets/images/Logo-Saint-Vincent.png" alt="logo Saint-Vincent">
+            <div>
+                <h1>Math Index</h1>
+                <h3>Lycée Saint-Vincent-Senlis</h3>
+            </div>
+        </div>
+        <div class="slide home">
+            <img src="assets/images/Logo-Home.png" alt="Logo home">
+            <h3>Accueil</h3>
+        </div>
+        <div class="slide search">
+            <img src="assets/images/Logo-loupe.png" alt="Logo loupe">
+            <h3>Recherche</h3>
+        </div>
+        <div class="slide math">
+            <img src="assets/images/logo-fonction.png" alt="Logo fonction Mathématique">
+            <h3>Mathématique</h3>
+        </div>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    require_once("components/slide-bar.php");
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -5,36 +9,16 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="assets/css/styleIndex.css" rel="stylesheet">
+    <link href="assets/css/style-Index.css" rel="stylesheet">
+    <link href="assets/css/style-slide-bar.css" rel="stylesheet">
     <title>Math Index</title>
 </head>
 
 <body>
-    <div class="slideBar"> <!--For the bar on the left -->
-        <div class="slidetitle">
-            <img src="assets/images/Logo-Saint-Vincent.png" alt="logo Saint-Vincent">
-            <div>
-                <h1>Math Index</h1>
-                <h3>Lycée Saint-Vincent-Senlis</h3>
-            </div>
-        </div>
-        <div class="slide home">
-            <img src="assets/images/Logo-Home.png" alt="Logo home">
-            <h3>Accueil</h3>
-        </div>
-        <div class="slide search">
-            <img src="assets/images/Logo-loupe.png" alt="Logo loupe">
-            <h3>Recherche</h3>
-        </div>
-        <div class=" slide math">
-            <img src="assets/images/logo-fonction.png" alt="Logo fonction Mathématique">
-            <h3>Mathématique</h3>
-        </div>
-    </div>
     <div class="container">
         <div class="connect">
             <img src="assets/images/Logo login.png" alt="logo connexion"><!--Rectangle on the top with the "Connexion title"-->
