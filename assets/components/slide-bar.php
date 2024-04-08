dbca97126a9fdeee7a9ef182c01e6b07ad247022
Noah <noah.bonnaventure@outlook.com>
2024-04-08 14:57:57 +0200

    ajout href mathématique dans le fichier de la navbar php

--- assets/components/slide-bar.php
+++ assets/components/slide-bar.php
@@ -28,7 +28,7 @@
 
         <div class="slide math">
             <img src="../assets/images/logo-fonction.png" alt="Logo fonction Mathématique">
-            <h3><a href="">Mathématique</a></h3>
+            <h3><a href="mathematique.php">Mathématique</a></h3>
         </div>
     </div>
 </body>
